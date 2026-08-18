36e1832850dac3d0ea8e44eebb5a3eff4679ac73
aszlig <aszlig@nix.build>
2026-08-18 05:56:46 +0200

    Add patched Pebble with deterministic root CA cert
    Testing ACME in upstream NixOS is a little bit cumbersome, because only
    the web frontend (WFE) certificate is can be referenced via Nix and the
    actual CA certificate is generated by Pebble at runtime.
    
    This means that in order to verify an issued certificate, we *also* need
    to fetch the root CA certificate at runtime, eg. via curl to
    https://acme.test:15000/roots/0, which makes certain configurations very
    hard to change, especially if it's not (easily) possibly to override the
    CA path.
    
    To address this, we now generate Pebbles root CA cert in exactly the
    same way as we do for the minica CA cert. This means that we just need
    to add the root CA cert from minica-deterministic and all certificates
    we enroll via our new deterministic Pebble will validate against the
    root CA from minica-deterministic.
    
    Since we're already patching Pebble anyway, I also made sure that the
    default directory endpoint is served at /directory, so it's similar to
    the real Boulder deployment from LetsEncrypt.
    
    We also now use the real LE directory URL by default, so we don't need
    to alter security.acme.defaults.server anymore.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,8 @@
     inherit (nixpkgs) lib;
   in {
     overlays.default = final: prev: {
-      inherit (self.packages.${final.system}) minica-deterministic;
+      inherit (self.packages.${final.system})
+        minica-deterministic pebble-deterministic;
     };
 
     packages = lib.mapAttrs (system: pkgs: let
@@ -60,7 +61,71 @@
     in {
       default = self.packages.${system}.minica-deterministic;
       minica-deterministic = minica-deterministic // { inherit ca; };
+      pebble-deterministic = patchedPkgs.pebble.overrideAttrs (drv: {
+        patches = (drv.patches or []) ++ [ patches/pebble.patch ];
+      });
     }) nixpkgs.legacyPackages;
+
+    nixosModules.pebble-deterministic = { config, pkgs, ... }: let
+      cfg = config.test-support.pebble-deterministic;
+      inherit (self.packages.${pkgs.system})
+        minica-deterministic pebble-deterministic;
+    in {
+      options.test-support.pebble-deterministic = {
+        caCert = lib.mkOption {
+          type = lib.types.path;
+          readOnly = true;
+          default = "${minica-deterministic.ca}/cert.pem";
+          description = ''
+            A certificate file to use with the `nodes` attribute to inject the
+            test CA certificate used in the ACME server into
+            {option}`security.pki.certificateFiles`.
+          '';
+        };
+        caDomain = lib.mkOption {
+          type = lib.types.str;
+          default = "acme-v02.api.letsencrypt.org";
+          description = ''
+            A domain name to use with the `nodes` attribute to identify the CA
+            server.
+          '';
+        };
+      };
+
+      config.networking.firewall.enable = false;
+      config.systemd.services.pebble-deterministic = {
+        description = "Deterministic Pebble ACME server";
+        after = [ "network.target" ];
+        requiredBy = [ "multi-user.target" ];
+
+        environment = {
+          PEBBLE_VA_NOSLEEP = "1";
+          PEBBLE_WFE_NONCEREJECT = "0";
+        };
+
+        serviceConfig = {
+          RuntimeDirectory = "pebble-deterministic";
+          WorkingDirectory = "/run/pebble-deterministic";
+          ExecStart = let
+            wfeCert = minica-deterministic.ca.mkCert {
+              domain = cfg.caDomain;
+            };
+            configFile = pkgs.writers.writeJSON "pebble-deterministic.conf" {
+              pebble = {
+                listenAddress = "[::]:443";
+                managementListenAddress = "[::]:15000";
+                certificate = "${wfeCert}/cert.pem";
+                privateKey = "${wfeCert}/key.pem";
+                httpPort = 80;
+                tlsPort = 443;
+                ocspResponderURL = "http://${cfg.caDomain}:4002";
+                strict = true;
+              };
+            };
+          in "${pebble-deterministic}/bin/pebble -config ${configFile}";
+        };
+      };
+    };
 
     checks = lib.mapAttrs (system: pkgs: rec {
       build = self.packages.${system}.minica-deterministic;
@@ -108,6 +173,101 @@
         openssl verify -verbose -CAfile "$ca/cert.pem" "$domain2/cert.pem"
         touch "$out"
       '';
+
+      pebble = pkgs.testers.runNixOSTest {
+        name = "pebble-deterministic";
+
+        defaults = { nodes, ... }: {
+          networking.firewall.enable = false;
+          networking.nameservers = [
+            nodes.dns.networking.primaryIPAddress
+            nodes.dns.networking.primaryIPv6Address
+          ];
+          security.pki.certificateFiles = [
+            nodes.acme.test-support.pebble-deterministic.caCert
+          ];
+        };
+
+        nodes = {
+          acme.imports = [ self.nixosModules.pebble-deterministic ];
+          client = {};
+          dns = { lib, nodes, config, ... }: {
+            services.bind.enable = true;
+            services.bind.cacheNetworks = lib.mkForce [ "any" ];
+            services.bind.forwarders = lib.mkForce [];
+            services.bind.extraOptions = "empty-zones-enable no;";
+            services.bind.zones = let
+              extractA = fqdn: nodecfg: ''
+                ${fqdn}. IN A ${nodecfg.networking.primaryIPAddress}
+                ${fqdn}. IN AAAA ${nodecfg.networking.primaryIPv6Address}
+              '';
+
+              inherit (nodes.acme.test-support.pebble-deterministic) caDomain;
+
+              extractAsFromNode = node: let
+                inherit (node.services.nginx) virtualHosts;
+                extractFromVhost = fqdn: lib.const (extractA fqdn node);
+              in lib.mapAttrsToList extractFromVhost virtualHosts;
+              hasNginxEnabled = node: node.services.nginx.enable;
+              nginxNodes = lib.filterAttrs (lib.const hasNginxEnabled) nodes;
+              nginxNodeConfigs = lib.attrValues nginxNodes;
+              nginxARecords = lib.concatMap extractAsFromNode nginxNodeConfigs;
+
+            in lib.singleton {
+              name = ".";
+              master = true;
+              file = pkgs.writeText "root.zone" ''
+                $TTL 3600
+                . IN SOA ns.fakedns. admin.fakedns. ( 1 3h 1h 1w 1d )
+                . IN NS ns.fakedns.
+                ${extractA "ns.fakedns" config}
+                ${extractA caDomain nodes.acme}
+                ${lib.concatStrings nginxARecords}
+              '';
+            };
+          };
+        } // lib.mapAttrs (serverName: fqdn: {
+          security.acme.acceptTerms = true;
+          security.acme.defaults.email = "ssladmin@${fqdn}";
+          services.nginx.enable = true;
+          services.nginx.virtualHosts.${fqdn} = {
+            enableACME = true;
+            forceSSL = true;
+            locations."/".root = pkgs.writeTextDir "index.txt" ''
+              Hello from ${serverName}!
+            '';
+          };
+        }) {
+          server1 = "example.com";
+          server2 = "example.net";
+          server3 = "example.org";
+        };
+
+        testScript = ''
+          import shlex
+
+          def wait_until_cert_issued(node: BaseMachine, domain: str) -> None:
+            node.wait_for_unit('nginx.service')
+            unit = f'acme-order-renew-{domain}.service'
+            node.succeed(f'systemctl start {shlex.quote(unit)}')
+
+          acme.start()
+          dns.wait_for_unit('bind.service')
+          acme.wait_for_unit('multi-user.target')
+
+          start_all()
+          wait_until_cert_issued(server1, 'example.com')
+          wait_until_cert_issued(server2, 'example.net')
+          wait_until_cert_issued(server3, 'example.org')
+
+          result = client.succeed('curl -f https://example.com/index.txt')
+          t.assertEqual(result, 'Hello from server1!\n')
+          result = client.succeed('curl -f https://example.net/index.txt')
+          t.assertEqual(result, 'Hello from server2!\n')
+          result = client.succeed('curl -f https://example.org/index.txt')
+          t.assertEqual(result, 'Hello from server3!\n')
+        '';
+      };
     }) nixpkgs.legacyPackages;
   };
 }
